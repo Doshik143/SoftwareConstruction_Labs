fd37fa23e59af3252f0bf446f2dd39ff4247f3c2
Doshik143 <ipz232_ddo@student.ztu.edu.ua>
2025-04-23 14:38:10 +0300

    Lab4:Task3[Part2]

---
+++ lab-4/t3/t5/ClassLibrary/LightNodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public static class LightNodeExtensions
+    {
+        public static IEnumerable<LightNode> GetChildren(this LightElementNode node)
+        {
+            //ToSimplifyDemonstration
+            var field = typeof(LightElementNode).GetField("_children",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            return (List<LightNode>)field.GetValue(node);
+        }
+    }
+}
