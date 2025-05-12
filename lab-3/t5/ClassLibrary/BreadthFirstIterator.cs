9ac018cb62884341abf56eaaf887f2707535611f
Doshik143 <ipz232_ddo@student.ztu.edu.ua>
2025-05-12 04:32:55 +0300

    Iterator

---
+++ lab-3/t5/ClassLibrary/BreadthFirstIterator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class BreadthFirstIterator : ILightNodeIterator
+    {
+        private Queue<LightNode> _queue = new Queue<LightNode>();
+
+        public BreadthFirstIterator(LightNode root)
+        {
+            _queue.Enqueue(root);
+        }
+
+        public bool HasNext() => _queue.Count > 0;
+
+        public LightNode Next()
+        {
+            var current = _queue.Dequeue();
+
+            if (current is LightElementNode element)
+            {
+                for (int i = 0; i < element.ChildCount; i++)
+                {
+                    _queue.Enqueue(element.GetChild(i));
+                }
+            }
+
+            return current;
+        }
+    }
+}
---
+++ lab-3/t5/ClassLibrary/DepthFirstIterator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class DepthFirstIterator : ILightNodeIterator
+    {
+        private Stack<LightNode> _stack = new Stack<LightNode>();
+
+        public DepthFirstIterator(LightNode root)
+        {
+            _stack.Push(root);
+        }
+
+        public bool HasNext() => _stack.Count > 0;
+
+        public LightNode Next()
+        {
+            var current = _stack.Pop();
+
+            if (current is LightElementNode element)
+            {
+                for (int i = element.ChildCount - 1; i >= 0; i--)
+                {
+                    _stack.Push(element.GetChild(i));
+                }
+            }
+
+            return current;
+        }
+    }
+}
