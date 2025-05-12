dc9cea005fd1c34f500717bd9dcb88fa7c952001
Doshik143 <ipz232_ddo@student.ztu.edu.ua>
2025-05-12 22:01:56 +0300

    TestProgram+Readme

---
+++ lab-3/t5/ClassLibrary/CommandManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class CommandManager
+    {
+        private Stack<ICommand> _history = new Stack<ICommand>();
+
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+            _history.Push(command);
+        }
+
+        public void Undo()
+        {
+            if (_history.Count > 0)
+            {
+                _history.Pop().Undo();
+            }
+        }
+    }
+}
